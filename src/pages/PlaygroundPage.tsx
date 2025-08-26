11cdd08f2a0fd769292a6ac7cff8b66ea098d226
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-08-26 12:10:39 +0200

    chore: new item

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,9 @@
 // import Onboarding from "../components/Onboarding";
 
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return <ExpandableText text="short text" />;
 };
 
 export default PlaygroundPage;
