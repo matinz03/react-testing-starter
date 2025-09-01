b7641da19b70f0b5b8fdcf76b317bdf6635e5f6e
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-01 21:49:38 +0200

    feat: productList tests implementation

---
+++ tests/components/ProductList.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import ProductList from "../../src/components/ProductList";
+import { server } from "../mocks/server";
+import { http, HttpResponse } from "msw";
+describe("ProductList", () => {
+  it("should render the list of products", async () => {
+    render(<ProductList />);
+    const items = await screen.findAllByRole("listitem");
+    expect(items.length).toBeGreaterThan(0);
+  });
+  it("should render 'No products available.' when there are no products", async () => {
+    server.use(http.get("/products", () => HttpResponse.json([])));
+    render(<ProductList />);
+    const message = await screen.findByText(/no products /i);
+    expect(message).toBeInTheDocument();
+  });
+});
