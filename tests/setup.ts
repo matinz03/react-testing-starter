7615339100b8bad1baef1c511260cb7103be6380
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-01 10:59:23 +0200

    fix: ResizeObserver dependency install

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,6 @@
 import "@testing-library/jest-dom/vitest";
+import ResizeObserver from "resize-observer-polyfill";
+global.ResizeObserver = ResizeObserver;
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: vi.fn().mockImplementation((query) => ({
