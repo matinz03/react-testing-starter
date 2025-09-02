cc91081c5e178b0470011daeca9a544610914782
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-02 15:46:00 +0200

    feat: use db and further tests for ProductDetails

--- tests/components/ProductDetails.test.tsx
+++ tests/components/ProductDetails.test.tsx
@@ -1,13 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import ProductDetail from "../../src/components/ProductDetail";
-import { products } from "../data";
 import { server } from "../mocks/server";
 import { http, HttpResponse } from "msw";
+import { db } from "../mocks/db";
 describe("ProductDetails", () => {
+  let productId: number;
+  beforeAll(() => {
+    const product = db.product.create();
+    productId = product.id;
+  });
+  afterAll(() => {
+    db.product.delete({ where: { id: { equals: productId } } });
+  });
   it("should render the product details", async () => {
-    render(<ProductDetail productId={1} />);
-    expect(await screen.findByText(new RegExp(products[0].name)));
-    expect(await screen.findByText(new RegExp(products[0].price.toString())));
+    render(<ProductDetail productId={productId} />);
+    const product = db.product.findFirst({
+      where: { id: { equals: productId } },
+    })!;
+
+    expect(await screen.findByText(new RegExp(product.name)));
+    expect(await screen.findByText(new RegExp(product.price.toString())));
   });
   it("should render message if product is not found", async () => {
     server.use(http.get("/products/1", () => HttpResponse.json(null)));
@@ -17,5 +29,10 @@ describe("ProductDetails", () => {
   it("should render an error for invalid productId", async () => {
     render(<ProductDetail productId={0} />);
     expect(await screen.findByText(/invalid /i)).toBeInTheDocument();
+  });
+  it("should render an error message when there is an error", async () => {
+    server.use(http.get("/products/1", () => HttpResponse.error()));
+    render(<ProductDetail productId={1} />);
+    expect(await screen.findByText(/error/i));
   });
 });
