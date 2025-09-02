b3b54722880536bc6ee7029b17b5e76a616948f9
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-02 15:44:48 +0200

    feat: setup db

---
+++ tests/mocks/db.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { factory, primaryKey } from "@mswjs/data";
+import { faker } from "@faker-js/faker";
+export const db = factory({
+  product: {
+    id: primaryKey(faker.number.int),
+    name: faker.commerce.productName,
+    price: faker.number.int.bind({ min: 1, max: 100 }),
+  },
+});
