abb277bd694c1ff1b22dae179a3fbf8e3b0db24f
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-03 19:24:30 +0200

    feat: make relations between products and categories

--- tests/mocks/db.ts
+++ tests/mocks/db.ts
@@ -1,16 +1,21 @@
 /* eslint-disable @typescript-eslint/unbound-method */
-import { factory, primaryKey } from "@mswjs/data";
+import { factory, manyOf, oneOf, primaryKey } from "@mswjs/data";
 import { faker } from "@faker-js/faker";
 export const db = factory({
   category: {
     id: primaryKey(faker.number.int),
     name: faker.commerce.department,
+    products: manyOf("product"),
   },
 
   product: {
     id: primaryKey(faker.number.int),
     name: faker.commerce.productName,
     categoryId: faker.number.int,
+    category: oneOf("category"),
     price: faker.number.int.bind({ min: 1, max: 100 }),
   },
 });
+export const getProductsByCategory = (categoryId: number) => {
+  return db.product.findMany({ where: { categoryId: { equals: categoryId } } });
+};
