164a5934c00c29716387db9a956a6cfdaa66ade6
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-03 17:11:38 +0200

    feat: implement category

--- tests/mocks/db.ts
+++ tests/mocks/db.ts
@@ -2,9 +2,15 @@
 import { factory, primaryKey } from "@mswjs/data";
 import { faker } from "@faker-js/faker";
 export const db = factory({
+  category: {
+    id: primaryKey(faker.number.int),
+    name: faker.commerce.department,
+  },
+
   product: {
     id: primaryKey(faker.number.int),
     name: faker.commerce.productName,
+    categoryId: faker.number.int,
     price: faker.number.int.bind({ min: 1, max: 100 }),
   },
 });
