4dd228c117b07d1d376b33ab0be1f2f9f58d0cc0
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-04 15:59:37 +0200

    feat: implement Cart and Theme providers

--- tests/AllProviders.tsx
+++ tests/AllProviders.tsx
@@ -1,5 +1,7 @@
 import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { CartProvider } from "../src/providers/CartProvider";
+import { Theme } from "@radix-ui/themes";
 
 export const AllProviders = ({ children }: PropsWithChildren) => {
   const client = new QueryClient({
@@ -9,5 +11,12 @@ export const AllProviders = ({ children }: PropsWithChildren) => {
       },
     },
   });
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <CartProvider>
+        <Theme>{children}</Theme>
+      </CartProvider>
+      ,
+    </QueryClientProvider>
+  );
 };
